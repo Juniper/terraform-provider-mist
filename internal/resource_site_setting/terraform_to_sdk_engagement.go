e716030c56e69f286aded0535fa066b822e5268e
Thomas Munzer <tmunzer@juniper.net>
2024-07-22 12:16:41 +0200

    improve mapping error management

--- internal/resource_site_setting/terraform_to_sdk_engagement.go
+++ internal/resource_site_setting/terraform_to_sdk_engagement.go
@@ -13,32 +13,34 @@ import (
 
 func engagementDwellTagNamesTerraformToSdk(ctx context.Context, diags *diag.Diagnostics, d basetypes.ObjectValue) *models.SiteEngagementDwellTagNames {
 	data := models.SiteEngagementDwellTagNames{}
-	if d.IsNull() || d.IsUnknown() {
-		return &data
-	} else {
-		v := NewDwellTagNamesValueMust(d.AttributeTypes(ctx), d.Attributes())
-		data.Bounce = v.Bounce.ValueStringPointer()
-		data.Engaged = v.Engaged.ValueStringPointer()
-		data.Passerby = v.Passerby.ValueStringPointer()
-		data.Stationed = v.Stationed.ValueStringPointer()
-
-		return &data
+	if !d.IsNull() || !d.IsUnknown() {
+		v, e := NewDwellTagNamesValue(d.AttributeTypes(ctx), d.Attributes())
+		if e != nil {
+			diags.Append(e...)
+		} else {
+			data.Bounce = v.Bounce.ValueStringPointer()
+			data.Engaged = v.Engaged.ValueStringPointer()
+			data.Passerby = v.Passerby.ValueStringPointer()
+			data.Stationed = v.Stationed.ValueStringPointer()
+		}
 	}
+	return &data
 }
 
 func engagementDwellTagsTerraformToSdk(ctx context.Context, diags *diag.Diagnostics, d basetypes.ObjectValue) *models.SiteEngagementDwellTags {
 	data := models.SiteEngagementDwellTags{}
-	if d.IsNull() || d.IsUnknown() {
-		return &data
-	} else {
-		v := NewDwellTagsValueMust(d.AttributeTypes(ctx), d.Attributes())
-		data.Bounce = models.NewOptional(v.Bounce.ValueStringPointer())
-		data.Engaged = models.NewOptional(v.Engaged.ValueStringPointer())
-		data.Passerby = models.NewOptional(v.Passerby.ValueStringPointer())
-		data.Stationed = models.NewOptional(v.Stationed.ValueStringPointer())
-
-		return &data
+	if !d.IsNull() || !d.IsUnknown() {
+		v, e := NewDwellTagsValue(d.AttributeTypes(ctx), d.Attributes())
+		if e != nil {
+			diags.Append(e...)
+		} else {
+			data.Bounce = models.NewOptional(v.Bounce.ValueStringPointer())
+			data.Engaged = models.NewOptional(v.Engaged.ValueStringPointer())
+			data.Passerby = models.NewOptional(v.Passerby.ValueStringPointer())
+			data.Stationed = models.NewOptional(v.Stationed.ValueStringPointer())
+		}
 	}
+	return &data
 }
 
 func engagementTerraformToSdk(ctx context.Context, diags *diag.Diagnostics, d EngagementValue) *models.SiteEngagement {
