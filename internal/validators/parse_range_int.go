c083a0a81a1f67822bbb5273b37b14ce9c7c36ef
Thomas Munzer <tmunzer@juniper.net>
2024-07-30 01:31:34 +0200

    improve ParseRangeOfIntValidator

--- internal/validators/parse_range_int.go
+++ internal/validators/parse_range_int.go
@@ -31,6 +31,16 @@ func (o ParseRangeOfIntValidator) ValidateString(_ context.Context, req validato
 	}
 
 	values := strings.Split(req.ConfigValue.ValueString(), "-")
+
+	if len(values) != 2 {
+		resp.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
+			req.Path,
+			fmt.Sprintf("must be a range of Integers between %s and %s (e.g. %s-%s)", strconv.Itoa(o.min), strconv.Itoa(o.max), strconv.Itoa(o.min), strconv.Itoa(o.max)),
+			req.ConfigValue.ValueString(),
+		))
+		return
+	}
+
 	for _, str_value := range values {
 		int_value, e := strconv.Atoi(str_value)
 		if e != nil || int_value < o.min || int_value > o.max {
@@ -41,6 +51,17 @@ func (o ParseRangeOfIntValidator) ValidateString(_ context.Context, req validato
 			))
 			return
 		}
+	}
+
+	value_one, _ := strconv.Atoi(values[0])
+	value_two, _ := strconv.Atoi(values[1])
+	if value_one >= value_two {
+		resp.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
+			req.Path,
+			fmt.Sprintf("must be a range of Integers between %s and %s, meaning the first value must be lower than the second value", strconv.Itoa(o.min), strconv.Itoa(o.max)),
+			req.ConfigValue.ValueString(),
+		))
+		return
 	}
 }
 
