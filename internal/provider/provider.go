198908adb2898909a78b9aa2d875f2034981b35f
Thomas Munzer <tmunzer@juniper.net>
2024-07-30 16:53:34 +0200

    provider - set "host" to "Required"

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -55,7 +55,7 @@ func (p *mistProvider) Schema(ctx context.Context, req provider.SchemaRequest, r
 			"host": schema.StringAttribute{
 				MarkdownDescription: "URL of the Mist Cloud, e.g. `api.mist.com`\n." +
 					"The preferred approach is to pass the credentials as environment variables `",
-				Optional: true,
+				Required: true,
 			},
 			"apitoken": schema.StringAttribute{
 				MarkdownDescription: "For Api Token authentication, the Mist API Token",
