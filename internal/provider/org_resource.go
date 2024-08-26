4ca2eb92408806d844a7536ff88100ae78fc1c45
Thomas Munzer <tmunzer@juniper.net>
2024-08-26 10:32:14 +0200

    fix mist_org category

--- internal/provider/org_resource.go
+++ internal/provider/org_resource.go
@@ -52,7 +52,7 @@ func (r *orgResource) Metadata(ctx context.Context, req resource.MetadataRequest
 
 func (r *orgResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		MarkdownDescription: docCategoryWan + "This resource manages the Mist Organization." +
+		MarkdownDescription: docCategoryOrg + "This resource manages the Mist Organization." +
 			"An organization usually represents a customer - which has inventories, licenses." +
 			"An Organization can contain multiple sites. A site usually represents a deployment at the same location (a campus, an office).",
 		Attributes: resource_org.OrgResourceSchema(ctx).Attributes,
