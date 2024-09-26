c2d05ad0894c3f16a2c916335feb300073990bb3
Thomas Munzer <tmunzer@juniper.net>
2024-09-26 19:59:22 +0200

    add new AllowedWhenValueIsNull  validator

---
+++ internal/validators/allwoed_when_value_is_null.go
@@ -0,0 +1,225 @@
+package mistvalidator
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+)
+
+var (
+	_ NineTypesValidator = AllowedWhenValueIsNullValidator{}
+)
+
+type AllowedWhenValueIsNullValidator struct {
+	expression path.Expression
+}
+
+type AllowedWhenValueIsNullRequest struct {
+	Config         tfsdk.Config
+	ConfigValue    attr.Value
+	Path           path.Path
+	PathExpression path.Expression
+}
+
+type AllowedWhenValueIsNullResponse struct {
+	Diagnostics diag.Diagnostics
+}
+
+func (o AllowedWhenValueIsNullValidator) Description(_ context.Context) string {
+	return fmt.Sprintf("Ensures that a value is supplied when attribute %q is not defined", o.expression.String())
+}
+
+func (o AllowedWhenValueIsNullValidator) MarkdownDescription(ctx context.Context) string {
+	return o.Description(ctx)
+}
+
+func (o AllowedWhenValueIsNullValidator) Validate(ctx context.Context, req AllowedWhenValueIsNullRequest, resp *AllowedWhenValueIsNullResponse) {
+	// can't proceed while value is unknown
+	if req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	// if we have a value there's no need for further investigation
+	if !req.ConfigValue.IsNull() {
+		return
+	}
+
+	mergedExpressions := req.PathExpression.MergeExpressions(o.expression)
+
+	for _, expression := range mergedExpressions {
+		matchedPaths, diags := req.Config.PathMatches(ctx, expression)
+		resp.Diagnostics.Append(diags...)
+		if resp.Diagnostics.HasError() {
+			return
+		}
+
+		for _, mp := range matchedPaths {
+			// If the user specifies the same attribute this mist_validator is applied to,
+			// also as part of the input, skip it
+			if mp.Equal(req.Path) {
+				continue
+			}
+
+			var mpVal attr.Value
+			resp.Diagnostics.Append(req.Config.GetAttribute(ctx, mp, &mpVal)...)
+			if resp.Diagnostics.HasError() {
+				continue // Collect all errors
+			}
+
+			// Unknown attributes can't satisfy the valueIs condition
+			if !mpVal.IsUnknown() {
+				resp.Diagnostics.Append(validatordiag.InvalidAttributeCombinationDiagnostic(
+					req.Path,
+					fmt.Sprintf("attribute %s is only allowed when %s is not set, got: %s", req.Path, mp, mpVal.String()),
+				))
+			}
+		}
+	}
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateBool(ctx context.Context, req validator.BoolRequest, resp *validator.BoolResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateFloat64(ctx context.Context, req validator.Float64Request, resp *validator.Float64Response) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateInt64(ctx context.Context, req validator.Int64Request, resp *validator.Int64Response) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateList(ctx context.Context, req validator.ListRequest, resp *validator.ListResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateMap(ctx context.Context, req validator.MapRequest, resp *validator.MapResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateNumber(ctx context.Context, req validator.NumberRequest, resp *validator.NumberResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateObject(ctx context.Context, req validator.ObjectRequest, resp *validator.ObjectResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateSet(ctx context.Context, req validator.SetRequest, resp *validator.SetResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func (o AllowedWhenValueIsNullValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+	validateReq := AllowedWhenValueIsNullRequest{
+		Config:         req.Config,
+		ConfigValue:    req.ConfigValue,
+		Path:           req.Path,
+		PathExpression: req.PathExpression,
+	}
+
+	validateResp := &AllowedWhenValueIsNullResponse{}
+
+	o.Validate(ctx, validateReq, validateResp)
+
+	resp.Diagnostics.Append(validateResp.Diagnostics...)
+}
+
+func AllowedWhenValueIsNull(expression path.Expression) AllowedWhenValueIsNullValidator {
+	return AllowedWhenValueIsNullValidator{
+		expression: expression,
+	}
+}
