e7d9e97981a19666bc75583ddb8dcc90ab75520b
Thomas Munzer <tmunzer@juniper.net>
2025-02-14 16:33:53 +0100

    fix for #76
    fix race condition issue when mulitple sites are assigned to/unassigned from the same site group at the same time

--- internal/provider/site_resource.go
+++ internal/provider/site_resource.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	mist_api_error "github.com/Juniper/terraform-provider-mist/internal/commons/api_response_error"
 	"github.com/Juniper/terraform-provider-mist/internal/resource_site"
@@ -17,9 +18,10 @@ import (
 )
 
 var (
-	_ resource.Resource                = &siteResource{}
-	_ resource.ResourceWithConfigure   = &siteResource{}
-	_ resource.ResourceWithImportState = &siteResource{}
+	_            resource.Resource                = &siteResource{}
+	_            resource.ResourceWithConfigure   = &siteResource{}
+	_            resource.ResourceWithImportState = &siteResource{}
+	resourceLock sync.Mutex
 )
 
 func NewSiteResource() resource.Resource {
@@ -44,7 +46,6 @@ func (r *siteResource) Configure(ctx context.Context, req resource.ConfigureRequ
 		)
 		return
 	}
-
 	r.client = client
 }
 func (r *siteResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
@@ -60,8 +61,15 @@ func (r *siteResource) Schema(ctx context.Context, req resource.SchemaRequest, r
 }
 
 func (r *siteResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	tflog.Info(ctx, "Starting Site Create")
 	var plan, state resource_site.SiteModel
+
+	// the resourceLock is required to avoid concurent sitegroup assign/unassign to
+	// different sites. If the same sitegroup is assigned to multiple sites at the
+	// same time, the `site_ids` atribute may not be updated correctly in the Mist
+	// backend
+	// by using this approach, the provider is creating/updating/deleting each sites
+	// sequentially
+	resourceLock.Lock()
 
 	diags := req.Plan.Get(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
@@ -83,7 +91,10 @@ func (r *siteResource) Create(ctx context.Context, req resource.CreateRequest, r
 		return
 	}
 	tflog.Info(ctx, "Starting Site Create for Org "+plan.OrgId.ValueString())
+
 	data, err := r.client.OrgsSites().CreateOrgSite(ctx, orgId, site)
+	// The API call has been done, we can unlock the resource
+	resourceLock.Unlock()
 
 	api_err := mist_api_error.ProcessApiError(ctx, data.Response.StatusCode, data.Response.Body, err)
 	if api_err != "" {
@@ -152,7 +163,14 @@ func (r *siteResource) Read(ctx context.Context, req resource.ReadRequest, resp 
 
 func (r *siteResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var state, plan resource_site.SiteModel
-	tflog.Info(ctx, "Starting Site Update")
+
+	// the resourceLock is required to avoid concurent sitegroup assign/unassign to
+	// different sites. If the same sitegroup is assigned to multiple sites at the
+	// same time, the `site_ids` atribute may not be updated correctly in the Mist
+	// backend
+	// by using this approach, the provider is creating/updating/deleting each sites
+	// sequentially
+	resourceLock.Lock()
 
 	diags := resp.State.Get(ctx, &state)
 	resp.Diagnostics.Append(diags...)
@@ -179,8 +197,11 @@ func (r *siteResource) Update(ctx context.Context, req resource.UpdateRequest, r
 		)
 		return
 	}
+
 	tflog.Info(ctx, "Starting Site Update for Site "+state.Id.ValueString())
 	data, err := r.client.Sites().UpdateSiteInfo(ctx, siteId, site)
+	// The API call has been done, we can unlock the resource
+	resourceLock.Unlock()
 
 	api_err := mist_api_error.ProcessApiError(ctx, data.Response.StatusCode, data.Response.Body, err)
 	if api_err != "" {
@@ -208,6 +229,14 @@ func (r *siteResource) Update(ctx context.Context, req resource.UpdateRequest, r
 func (r *siteResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
 	var state resource_site.SiteModel
 
+	// the resourceLock is required to avoid concurent sitegroup assign/unassign to
+	// different sites. If the same sitegroup is assigned to multiple sites at the
+	// same time, the `site_ids` atribute may not be updated correctly in the Mist
+	// backend
+	// by using this approach, the provider is creating/updating/deleting each sites
+	// sequentially
+	resourceLock.Lock()
+
 	diags := resp.State.Get(ctx, &state)
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
@@ -222,8 +251,12 @@ func (r *siteResource) Delete(ctx context.Context, req resource.DeleteRequest, r
 		)
 		return
 	}
+
 	tflog.Info(ctx, "Starting Site Delete: site_id "+state.Id.ValueString())
 	data, err := r.client.Sites().DeleteSite(ctx, siteId)
+	// The API call has been done, we can unlock the resource
+	resourceLock.Unlock()
+
 	api_err := mist_api_error.ProcessApiError(ctx, data.StatusCode, data.Body, err)
 	if data.StatusCode != 404 && api_err != "" {
 		resp.Diagnostics.AddError(
