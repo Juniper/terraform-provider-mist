57f3bbbfabcd08d5879ea0d3fbf289d813eadf7b
Thomas Munzer <tmunzer@juniper.net>
2025-02-19 17:39:04 +0100

    Update tfplugindocs.sh

--- scripts/tfplugindocs.sh
+++ scripts/tfplugindocs.sh
@@ -6,6 +6,8 @@ grep -E '^subcategory: ""$' docs/data-sources/* && echo "missing subcategory" &&
 
 grep -E '^subcategory: ""$' docs/resources//* && echo "missing subcategory" && exit 1
 
+grep -E '^subcategory: ""$' docs/functions//* && echo "missing subcategory" && exit 1
+
 git update-index --refresh || exit 1
 
 git diff-index --quiet HEAD -- || exit 1
