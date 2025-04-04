8cf63ab785f8c06dfb53b4c30dbd6025e83f1936
Thomas Munzer <thomas.munzer@gmail.com>
2025-04-04 20:53:07 +0200

    fix wording

--- internal/validators/allowed_when_value_is_in.go
+++ internal/validators/allowed_when_value_is_in.go
@@ -84,7 +84,7 @@ func (o AllowedWhenValueIsInValidator) Validate(ctx context.Context, req Allowed
 			if !allowed {
 				resp.Diagnostics.Append(validatordiag.InvalidAttributeCombinationDiagnostic(
 					req.Path,
-					fmt.Sprintf("Attribute \"%s\" is only allowed when \"%s\" has value in %s, got: %s", req.Path, mp, o.Values, mpVal.String()),
+					fmt.Sprintf("Attribute \"%s\" is only allowed when \"%s\" has value %s, got: %s", req.Path, mp, o.Values, mpVal.String()),
 				))
 			}
 		}
