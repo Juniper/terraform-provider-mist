609a979e8205a86945b4430479581379028c344a
Kevin-DeJong <kdejong@juniper.net>
2025-06-26 16:32:05 +0200

    remove api token

--- configuration/aws-staging.tf
+++ configuration/aws-staging.tf
@@ -9,5 +9,5 @@ terraform {
 
 provider "mist" {
   host = "api.mistsys.com"
-  apitoken = "pe7stWeewIecNLcRRGnF9T98AQ8JTZOGCwja9Iu8xQS9KhjG9bKCCMnaWNsXd9qpfhQGTRh17ZUg6qcPDmOFxgXKYDVoE28h"
+  apitoken = ""
 }
