9ae8b42da211f0b4034013ba673f8c9cacc47969
Thomas Munzer <tmunzer@juniper.net>
2025-08-18 20:55:22 +0200

    fix wlan.app_limit
    fix for #128

--- internal/resource_site_wlan/terraform_to_sdk_app_liimit.go
+++ internal/resource_site_wlan/terraform_to_sdk_app_liimit.go
@@ -1,6 +1,7 @@
 package resource_site_wlan
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/tmunzer/mistapi-go/mistapi/models"
 )
 
@@ -11,13 +12,13 @@ func appLimitTerraformToSdk(plan AppLimitValue) *models.WlanAppLimit {
 	appLimit := make(map[string]int)
 	for k, v := range plan.Apps.Elements() {
 		var vInterface interface{} = v
-		appLimit[k] = int(vInterface.(int64))
+		appLimit[k] = int(vInterface.(basetypes.Int64Value).ValueInt64())
 	}
 
 	wxtagsLimit := make(map[string]int)
 	for k, v := range plan.WxtagIds.Elements() {
 		var vInterface interface{} = v
-		wxtagsLimit[k] = int(vInterface.(int64))
+		wxtagsLimit[k] = int(vInterface.(basetypes.Int64Value).ValueInt64())
 	}
 
 	data.Apps = appLimit
