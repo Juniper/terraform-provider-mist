b3c48e9cae8e77df2b1acdc858cc22c5c2916876
Willem Prins <wprins@juniper.net>
2025-09-04 16:01:00 +0200

    Added test for site resource

---
+++ internal/provider/fixtures/site_resource/site_resource_config.tf
@@ -0,0 +1,20 @@
+# Comprehensive Site Resource Test Configuration
+# This fixture tests all major fields of the Site resource
+
+name                    = "Comprehensive_Test_Site"
+address                 = "123 Test Street, San Francisco, CA 94105"
+alarmtemplate_id       = "00000000-0000-0000-1000-5c5b35000010"
+aptemplate_id          = "00000000-0000-0000-2000-5c5b35000020"
+country_code           = "US"
+gatewaytemplate_id     = "00000000-0000-0000-3000-5c5b35000030"
+latlng = {
+  lat = 37.7749
+  lng = -122.4194
+}
+networktemplate_id     = "00000000-0000-0000-4000-5c5b35000040"
+notes                  = "This is a comprehensive test site with all major configuration options"
+rftemplate_id          = "00000000-0000-0000-5000-5c5b35000050"
+secpolicy_id           = "00000000-0000-0000-6000-5c5b35000060"
+sitegroup_ids          = ["00000000-0000-0000-7000-5c5b35000070", "00000000-0000-0000-7000-5c5b35000071"]
+sitetemplate_id        = "00000000-0000-0000-8000-5c5b35000080"
+timezone               = "America/Los_Angeles"
--- internal/provider/site_resource_test.go
+++ internal/provider/site_resource_test.go
@@ -35,55 +35,53 @@ func TestSiteModel(t *testing.T) {
 		},
 	}
 
-	b, err := os.ReadFile("fixtures/site_resource/site_config.tf")
+	// Load fixture data
+	b, err := os.ReadFile("fixtures/site_resource/site_resource_config.tf")
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	str := string(b) // convert content to a 'string'
+	str := string(b)
 	fixtures := strings.Split(str, "␞")
 
 	for i, fixture := range fixtures {
-
-		var FixtureSiteModel SiteModel
-		err = hcl.Decode(&FixtureSiteModel, fixture)
+		fixtureSiteModel := SiteModel{}
+		err = hcl.Decode(&fixtureSiteModel, fixture)
 		if err != nil {
 			fmt.Printf("error decoding hcl: %s\n", err)
 		}
 
-		FixtureSiteModel.OrgId = GetTestOrgId()
+		fixtureSiteModel.OrgId = GetTestOrgId()
 
 		testCases[fmt.Sprintf("fixture_case_%d", i)] = testCase{
 			steps: []testStep{
 				{
-					config: FixtureSiteModel,
+					config: fixtureSiteModel,
 				},
 			},
 		}
 	}
 
+	resourceType := "site"
 	for tName, tCase := range testCases {
 		t.Run(tName, func(t *testing.T) {
-			resourceType := "site"
-
 			steps := make([]resource.TestStep, len(tCase.steps))
 			for i, step := range tCase.steps {
-				config := step.config
-
+				// Generate Terraform configuration using automated HCL generation
 				f := hclwrite.NewEmptyFile()
-				gohcl.EncodeIntoBody(&config, f.Body())
-				configStr := Render(resourceType, tName, string(f.Bytes()))
+				gohcl.EncodeIntoBody(&step.config, f.Body())
+				combinedConfig := Render("site", tName, string(f.Bytes()))
 
-				checks := config.testChecks(t, resourceType, tName)
+				checks := step.config.testChecks(t, resourceType, tName)
 				chkLog := checks.string()
 				stepName := fmt.Sprintf("test case %s step %d", tName, i+1)
 
 				// log config and checks here
-				t.Logf("\n// ------ begin config for %s ------\n%s// -------- end config for %s ------\n\n", stepName, configStr, stepName)
+				t.Logf("\n// ------ begin config for %s ------\n%s// -------- end config for %s ------\n\n", stepName, combinedConfig, stepName)
 				t.Logf("\n// ------ begin checks for %s ------\n%s// -------- end checks for %s ------\n\n", stepName, chkLog, stepName)
 
 				steps[i] = resource.TestStep{
-					Config: configStr,
+					Config: combinedConfig,
 					Check:  resource.ComposeAggregateTestCheckFunc(checks.checks...),
 				}
 			}
@@ -96,13 +94,18 @@ func TestSiteModel(t *testing.T) {
 	}
 }
 
-func (s *SiteModel) testChecks(t testing.TB, rType, rName string) testChecks {
-	checks := newTestChecks(PrefixProviderName(rType) + "." + rName)
+func (s *SiteModel) testChecks(t testing.TB, rType, tName string) testChecks {
+	checks := newTestChecks(PrefixProviderName(rType) + "." + tName)
+
+	// Computed field (id) - use TestCheckResourceAttrSet
+	checks.append(t, "TestCheckResourceAttrSet", "id")
+
+	// Required string fields
 	checks.append(t, "TestCheckResourceAttr", "address", s.Address)
 	checks.append(t, "TestCheckResourceAttr", "name", s.Name)
 	checks.append(t, "TestCheckResourceAttr", "org_id", s.OrgId)
 
-	// Conditional checks for optional parameters
+	// Optional string fields
 	if s.AlarmtemplateId != nil {
 		checks.append(t, "TestCheckResourceAttr", "alarmtemplate_id", *s.AlarmtemplateId)
 	}
@@ -114,10 +117,6 @@ func (s *SiteModel) testChecks(t testing.TB, rType, rName string) testChecks {
 	}
 	if s.GatewaytemplateId != nil {
 		checks.append(t, "TestCheckResourceAttr", "gatewaytemplate_id", *s.GatewaytemplateId)
-	}
-	if s.Latlng != nil {
-		checks.append(t, "TestCheckResourceAttr", "latlng.lat", fmt.Sprintf("%g", s.Latlng.Lat))
-		checks.append(t, "TestCheckResourceAttr", "latlng.lng", fmt.Sprintf("%g", s.Latlng.Lng))
 	}
 	if s.NetworktemplateId != nil {
 		checks.append(t, "TestCheckResourceAttr", "networktemplate_id", *s.NetworktemplateId)
@@ -131,16 +130,29 @@ func (s *SiteModel) testChecks(t testing.TB, rType, rName string) testChecks {
 	if s.SecpolicyId != nil {
 		checks.append(t, "TestCheckResourceAttr", "secpolicy_id", *s.SecpolicyId)
 	}
-	if len(s.SitegroupIds) > 0 {
-		for i, id := range s.SitegroupIds {
-			checks.append(t, "TestCheckResourceAttr", fmt.Sprintf("sitegroup_ids.%d", i), id)
-		}
-	}
 	if s.SitetemplateId != nil {
 		checks.append(t, "TestCheckResourceAttr", "sitetemplate_id", *s.SitetemplateId)
 	}
 	if s.Timezone != nil {
 		checks.append(t, "TestCheckResourceAttr", "timezone", *s.Timezone)
+	}
+
+	// Array fields - sitegroup_ids
+	if len(s.SitegroupIds) > 0 {
+		checks.append(t, "TestCheckResourceAttr", "sitegroup_ids.#", fmt.Sprintf("%d", len(s.SitegroupIds)))
+		for i, id := range s.SitegroupIds {
+			checks.append(t, "TestCheckResourceAttr", fmt.Sprintf("sitegroup_ids.%d", i), id)
+		}
+	}
+
+	// Complex object fields - latlng
+	if s.Latlng != nil {
+		if s.Latlng.Lat != 0 {
+			checks.append(t, "TestCheckResourceAttr", "latlng.lat", fmt.Sprintf("%g", s.Latlng.Lat))
+		}
+		if s.Latlng.Lng != 0 {
+			checks.append(t, "TestCheckResourceAttr", "latlng.lng", fmt.Sprintf("%g", s.Latlng.Lng))
+		}
 	}
 
 	return checks
