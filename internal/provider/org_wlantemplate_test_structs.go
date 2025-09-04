19d9cda181f6534cc369384423f112e369fdfc28
Willem Prins <wprins@juniper.net>
2025-09-04 15:28:38 +0200

    Added fixture tests for org wlan template.

--- internal/provider/org_wlantemplate_test_structs.go
+++ internal/provider/org_wlantemplate_test_structs.go
@@ -5,17 +5,18 @@ type OrgWlantemplateModel struct {
 	DeviceprofileIds      []string         `hcl:"deviceprofile_ids"`
 	Exceptions            *ExceptionsValue `hcl:"exceptions"`
 	FilterByDeviceprofile *bool            `hcl:"filter_by_deviceprofile"`
+	Id                    *string          `hcl:"id"`
 	Name                  string           `hcl:"name"`
 	OrgId                 string           `hcl:"org_id"`
 }
 
 type AppliesValue struct {
-	OrgId        *string  `cty:"org_id"`
-	SiteIds      []string `cty:"site_ids"`
-	SitegroupIds []string `cty:"sitegroup_ids"`
+	OrgId        *string  `hcl:"org_id" cty:"org_id"`
+	SiteIds      []string `hcl:"site_ids" cty:"site_ids"`
+	SitegroupIds []string `hcl:"sitegroup_ids" cty:"sitegroup_ids"`
 }
 
 type ExceptionsValue struct {
-	SiteIds      []string `cty:"site_ids"`
-	SitegroupIds []string `cty:"sitegroup_ids"`
+	SiteIds      []string `hcl:"site_ids" cty:"site_ids"`
+	SitegroupIds []string `hcl:"sitegroup_ids" cty:"sitegroup_ids"`
 }
