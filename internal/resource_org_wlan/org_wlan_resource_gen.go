0d3def1c39bc72c6a818ff008a9124762b70996b
Thomas Munzer <tmunzer@juniper.net>
2025-10-02 17:37:15 +0200

    update attributes description

--- internal/resource_org_wlan/org_wlan_resource_gen.go
+++ internal/resource_org_wlan/org_wlan_resource_gen.go
@@ -332,7 +332,6 @@ func OrgWlanResourceSchema(ctx context.Context) schema.Schema {
 						},
 						Optional: true,
 						Validators: []validator.List{
-							listvalidator.SizeAtLeast(1),
 							listvalidator.UniqueValues(),
 						},
 					},
@@ -2121,8 +2120,8 @@ func OrgWlanResourceSchema(ctx context.Context) schema.Schema {
 			},
 			"portal_api_secret": schema.StringAttribute{
 				Computed:            true,
-				Description:         "APi secret (auto-generated) that can be used to sign guest authorization requests",
-				MarkdownDescription: "APi secret (auto-generated) that can be used to sign guest authorization requests",
+				Description:         "API secret (auto-generated) that can be used to sign guest authorization requests, only generated when auth is set to `external`",
+				MarkdownDescription: "API secret (auto-generated) that can be used to sign guest authorization requests, only generated when auth is set to `external`",
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
@@ -2144,7 +2143,9 @@ func OrgWlanResourceSchema(ctx context.Context) schema.Schema {
 				},
 			},
 			"portal_sso_url": schema.StringAttribute{
-				Computed: true,
+				Computed:            true,
+				Description:         "URL used in the SSO process, auto-generated when auth is set to `sso`",
+				MarkdownDescription: "URL used in the SSO process, auto-generated when auth is set to `sso`",
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
 				},
