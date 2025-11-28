4dff913bc86993a489f4a39f8c1d168a12eb4886
Kevin-DeJong <kdejong@juniper.net>
2025-11-28 08:38:55 +0200

    MIST-171099 fix org service issues (#159)

--- internal/commons/utils/containers.go
+++ internal/commons/utils/containers.go
@@ -12,83 +12,54 @@ import (
 	"github.com/tmunzer/mistapi-go/mistapi/models"
 )
 
-func WlanVlanAsString(vlanId models.WlanVlanIdWithVariable) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
+// StringOrNumberContainer is an interface for SDK types that can hold either a string or number value
+type StringOrNumberContainer interface {
+	AsString() (*string, bool)
+	AsNumber() (*int, bool)
+}
+
+// ContainerAsString converts any SDK container type that implements StringOrNumberContainer to a Terraform StringValue
+func ContainerAsString[T StringOrNumberContainer](container T) basetypes.StringValue {
+	if v, ok := container.AsString(); ok && v != nil {
 		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
+	} else if v, ok := container.AsNumber(); ok && v != nil {
+		return types.StringValue(fmt.Sprintf("%d", *v))
 	} else {
 		return types.StringNull()
 	}
 }
+
+// Legacy function wrappers for backward compatibility
+func WlanVlanAsString(vlanId models.WlanVlanIdWithVariable) basetypes.StringValue {
+	return ContainerAsString(&vlanId)
+}
+
 func VlanAsString(vlanId models.VlanIdWithVariable) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func GatewayVlanAsString(vlanId models.GatewayPortVlanIdWithVariable) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func WlanDynamicVlanAsString(vlanId models.WlanDynamicVlanDefaultVlanId) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func WlanDynamicVlanDefaultVlanIdDeprecatedAsString(vlanId models.WlanDynamicVlanDefaultVlanIdDeprecated) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func PskVlanAsString(vlanId models.PskVlanId) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func SwitchPortUsageReauthIntervalAsString(vlanId models.SwitchPortUsageReauthInterval) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func DscpAsString(vlanId models.Dscp) basetypes.StringValue {
-	if v, ok := vlanId.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := vlanId.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&vlanId)
 }
 
 func WlanBonjourAdditionalVlanIdsAsArrayOfString(diags *diag.Diagnostics, vlanIds models.AdditionalVlanIds) basetypes.ListValue {
@@ -132,200 +103,81 @@ func WlanVlanIdsAsArrayOfString(diags *diag.Diagnostics, vlanIds *models.WlanVla
 }
 
 func BgpAsAsString(bgpAs *models.BgpAs) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func BgpLocalAsAsString(bgpAs *models.BgpLocalAs) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func RadiusCoaPortAsString(bgpAs *models.RadiusCoaPort) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func RadiusAcctPortAsString(bgpAs *models.RadiusAcctPort) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func RadiusAuthPortAsString(bgpAs *models.RadiusAuthPort) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func SyslogFilesAsString(bgpAs *models.RemoteSyslogArchiveFiles) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func SyslogPortAsString(bgpAs *models.RemoteSyslogServerPort) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func SwitchPortUsageMacLimitAsString(bgpAs *models.SwitchPortUsageMacLimit) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
+
 func SwitchPortOverwriteUsageMacLimitAsString(bgpAs *models.SwitchPortUsageMacLimitOverwrite) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func SwitchPortUsageMtuAsString(bgpAs *models.SwitchPortUsageMtu) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func SwitchMgmtMxedgeProxyPortsAsString(bgpAs *models.SwitchMgmtMxedgeProxyPort) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func GatewayPortConfigRethIdxAsString(bgpAs *models.GatewayPortConfigRethIdx) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func RadsecIdleTimeoutAsString(bgpAs *models.RadsecIdleTimeout) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func SponsorLinkValidityDurationAsString(bgpAs *models.SponsorLinkValidityDuration) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func GbpTagAsString(gbpTag models.NacTagGbpTag) basetypes.StringValue {
-	if v, ok := gbpTag.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := gbpTag.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(&gbpTag)
 }
 
 func WlanLimitAsString(bgpAs *models.WlanLimit) basetypes.StringValue {
-	if v, ok := bgpAs.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := bgpAs.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(bgpAs)
 }
 
 func ServiceDscpAsString(container *models.ServiceDscp) basetypes.StringValue {
-	if v, ok := container.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := container.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(container)
 }
 
 func ServiceMaxJitterAsString(container *models.ServiceMaxJitter) basetypes.StringValue {
-	if v, ok := container.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := container.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(container)
 }
 
 func ServiceMaxLatencyAsString(container *models.ServiceMaxLatency) basetypes.StringValue {
-	if v, ok := container.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := container.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(container)
 }
 
 func ServiceMaxLossAsString(container *models.ServiceMaxLoss) basetypes.StringValue {
-	if v, ok := container.AsString(); ok {
-		return types.StringValue(*v)
-	} else if v, ok := container.AsNumber(); ok {
-		return types.StringValue(fmt.Sprint(*v))
-	} else {
-		return types.StringNull()
-	}
+	return ContainerAsString(container)
 }
