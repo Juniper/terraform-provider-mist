e3b7628c2e91d3151e847863b8273e68d360bb6c
Kevin-DeJong <kdejong@juniper.net>
2025-12-16 13:55:49 +0200

    fixture update for mxedge_inventory

--- internal/provider/fixtures/org_mxedge_inventory_resource/org_mxedge_inventory_config.tf
+++ internal/provider/fixtures/org_mxedge_inventory_resource/org_mxedge_inventory_config.tf
@@ -1,5 +1,5 @@
 mxedges = {
-  "00000000-0000-0000-1000-02000098882d" = {
-    claim_code = "SebYGd0mzjZa1okk0A1y-PdNK0MjMPpCGu8o6TvGnpiV4fzuKrCEkjMBdvofBVjC"
+  "EKF4DSY7TZMDH9P" = {
+    # site_id = "{site_id}"
   }
 }
