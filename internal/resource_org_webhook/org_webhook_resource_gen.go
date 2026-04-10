22cf10cbb685ec5c390fc727216fcb8fa929abd1
Thomas Munzer <tmunzer@juniper.net>
2026-04-10 15:43:01 +0200

    add missing org_webhook topics

--- internal/resource_org_webhook/org_webhook_resource_gen.go
+++ internal/resource_org_webhook/org_webhook_resource_gen.go
@@ -4,7 +4,8 @@ package resource_org_webhook
 
 import (
 	"context"
-	"github.com/Juniper/terraform-provider-mist/internal/validators"
+
+	mistvalidator "github.com/Juniper/terraform-provider-mist/internal/validators"
 	"github.com/hashicorp/terraform-plugin-framework-validators/listvalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
@@ -158,6 +159,8 @@ func OrgWebhookResourceSchema(ctx context.Context) schema.Schema {
 							"device-updowns",
 							"guest-authorizations",
 							"mxedge-events",
+							"minis-reachability",
+							"minis-application",
 							"nac-accounting",
 							"nac-events",
 						),
