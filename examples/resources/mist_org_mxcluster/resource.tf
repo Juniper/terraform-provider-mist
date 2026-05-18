5c35b51ae423fdf60eb0c0ebc405cfdfa64f1b11
Kevin-DeJong <kdejong@juniper.net>
2026-05-18 11:59:43 +0200

    Remove hard coded org_id

--- examples/resources/mist_org_mxcluster/resource.tf
+++ examples/resources/mist_org_mxcluster/resource.tf
@@ -1,24 +1,14 @@
 
-terraform {
-  required_providers {
-    mist = {
-      source = "juniper/mist"
-    }
-  }
-}
-
-provider "mist" {
-  host = "api.mistsys.com"
-  # api_token can be set via MIST_API_TOKEN environment variable
-  # or uncomment below:
-  # api_token = "yEtsUU0JcxXpyFTZ6x4u4khaFXotuc3eW5UVhFZWkmOJi4DhUvZZaOmQGbB4Nu3GmBGoBgCKBC2PvPwGZSVet8iit0BwHeb3m"
-}
-
 # Resource for importing existing mxcluster
-# Import with: terraform import mist_org_mxcluster.existing_mxcluster "76bd3d9a-48b8-468f-83c1-6fb2544f7815.ee32b2a5-1c0f-44ae-8539-6cc7394870c7"
+# Import with: terraform import mist_org_mxcluster.existing_mxcluster "<org_id>.<mxcluster_id>"
 resource "mist_org_mxcluster" "existing_mxcluster" {
-  org_id = "76bd3d9a-48b8-468f-83c1-6fb2544f7815"
+  org_id = <org_id>
   name   = "edgey_cluster"
 }
 
 
+resource "mist_org_mxedge" "mxedge" {
+  org_id       = mist_org_mxcluster.existing_mxcluster.org_id
+  claim_code = <claim_code>
+  name         = "me1"
+}
